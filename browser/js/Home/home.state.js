2b7d08dbfed890e7d2ab1e2acf9305c5fcc7e74f
Wallaceness <ncw232@gmail.com>
2018-07-11 00:09:55 -0400

    removed transition page

--- browser/js/Home/home.state.js
+++
@@ -1,7 +0,0 @@
-app.config(function($stateProvider) {
-    $stateProvider.state("home", {
-        url: "",
-        templateUrl: "browser/js/Home/home.html",
-        controller: "homepage_ctrl"
-    })
-})
--- browser/js/about_me/about_me.state.js
+++ browser/js/about_me/about_me.state.js
@@ -1,6 +1,6 @@
 app.config(function($stateProvider) {
     $stateProvider.state("about_me", {
-        url: "about_me",
+        url: "",
         templateUrl: "browser/js/about_me/about_me.html",
         controller: "about_me_ctrl"
     })
