16d996cd5555863dbbd6218ee0e0ff09bab81df6
Wallaceness <ncw232@gmail.com>
2018-07-16 23:57:20 -0400

    remodelled website

--- browser/js/about_me/about_me.controller.js
+++ browser/js/about_me/about_me.controller.js
@@ -1,17 +1,20 @@
 app.controller("about_me_ctrl", function($scope) {
-    $scope.profile = { Name: "Nathaniel C. Wallace", "Preferred Name": "Nathan", Birthday: "December 19, 1990", Age: "26", Occupation: "Jr. Web Developer", "Born In": "Richmond, VA", "Lives In": "Staten Island, NY", Email: "ncw232@gmail.com" };
+    $scope.profile = { Name: "Nathaniel C. Wallace", "Preferred Name": "Nathan", Birthday: "December 19, 1990", Age: "Twenty-Seven", Occupation: "Jr. Web Developer", "Born In": "Richmond, VA", "Lives In": "Brooklyn, NY", Email: "ncw232@gmail.com" };
     $scope.skills = [
         ["HTML", "CSS", "Javascript", "JQuery"],
-        ["Angular 1", "Socket.io", "Node.js", "Postgres"],
-        ["SASS", "Git/Github", "Python 2.7", "Gulp"]
+        ["Angular 1", "Socket.io", "Node.js", "Sequel"],
+        ["SASS", "Git/Github", "Python", "Gulp"],
+        ["Webpack", "React", "Redux" ]
     ]
     $scope.education = [{ name: "Fullstack Academy of Code", location: "New York, NY", dates: "July-October, 2016", studied: "Web Development" },
         { name: "J. Sargent Reynolds Community College", location: "Richmond, VA", dates: "April 2008-October 2009", studied: "Business, Precalculus, Drafting, World History, Geography" },
         { name: "Adult Education Center", location: "Richmond, VA", dates: "February 2009", studied: "GED" }
     ];
-    $scope.work = [{ name: "Kroger", dates: "October 2010-June 2012", position: "Cashier, U-Scan Coordinator, Office Clerk" },
+    $scope.work = [{ name: "Quality Edgar Solutions", dates: "April 2017-Present", position: "Web Developer" },
+        { name: "Papa Johns", dates: "August 2014-February 2015", position: "Delivery Driver" },
         { name: "E-bay", dates: "November 2012-January 2013; December 2013; December 2015-February 2016", position: "Picker, Packer, Returns Associate" },
-        { name: "Papa Johns", dates: "August 2014-February 2015", position: "Delivery Driver" }
+        { name: "Kroger", dates: "October 2010-June 2012", position: "Cashier, U-Scan Coordinator, Office Clerk" },
+    
     ];
     $scope.show_work = false;
     $scope.show_education = false;
--- browser/js/links/links.controller.js
+++ browser/js/links/links.controller.js
@@ -3,6 +3,5 @@ app.controller("links_ctrl", function($scope, $sce) {
         { name: "My Stackoverflow Profile", link: $sce.trustAsResourceUrl("http://stackoverflow.com/users/7416146/wallaceness") },
         { name: "My Linkedin Profile", link: $sce.trustAsResourceUrl("https://www.linkedin.com/in/nathan-wallace-626b2326/") },
         { name: "My HackerRank Profile", link: $sce.trustAsResourceUrl("https://www.hackerrank.com/ncw232") },
-        { name: "My freelancer.com Profile", link: $sce.trustAsResourceUrl("https://www.freelancer.com/u/Wallaceness.html") }
     ]
 })
