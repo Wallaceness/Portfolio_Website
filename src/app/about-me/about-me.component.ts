2a294d57f1375a52060507eb5d501457c3ebccd4
Wallaceness <ncw232@gmail.com>
2019-08-14 17:31:59 -0400

    One last tweak.

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-me.component.css']
 })
 export class AboutMeComponent implements OnInit {
-	profile: Object = { Name: "Nathaniel C. Wallace", "Preferred Name": "Nathan", Birthday: "December 19, 1990", Age: "Twenty-Seven", Occupation: "Jr. Web Developer", "Born In": "Richmond, VA", "Lives In": "Brooklyn, NY", Email: "ncw232@gmail.com" };
+	profile: Object = [{ Name: "Nathaniel C. Wallace"}, {"Preferred Name": "Nathan"}, {Birthday: "December 19, 1990"}, {Age: "Twenty-Eight"}, {Occupation: "Web Developer"}, {"Born In": "Richmond, VA"}, {"Lives In": "Richmond, VA"}, {Email: "ncw232@gmail.com" }];
   skills: Object = [
         ["HTML", "CSS", "Javascript", "JQuery"],
         ["Angular 1", "Socket.io", "Node.js", "Sequel"],
@@ -17,7 +17,9 @@ export class AboutMeComponent implements OnInit {
         { name: "J. Sargent Reynolds Community College", location: "Richmond, VA", dates: "April 2008-October 2009", studied: "Business, Precalculus, Drafting, World History, Geography" },
         { name: "Adult Education Center", location: "Richmond, VA", dates: "February 2009", studied: "GED" }
     ];
-  work: Object = [{ name: "Quality Edgar Solutions", dates: "April 2017-Present", position: "Web Developer" },
+  work: Object = [
+        { name: "ChartIQ", dates: "April 2019-July 2019", position: "Software Developer" },
+        { name: "Quality Edgar Solutions", dates: "April 2017-December 2018", position: "Front-end Web Developer" },
         { name: "Papa Johns", dates: "August 2014-February 2015", position: "Delivery Driver" },
         { name: "E-bay", dates: "November 2012-January 2013; December 2013; December 2015-February 2016", position: "Picker, Packer, Returns Associate" },
         { name: "Kroger", dates: "October 2010-June 2012", position: "Cashier, U-Scan Coordinator, Office Clerk" },
