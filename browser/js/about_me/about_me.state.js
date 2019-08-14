19c5ad67fecdadf807e9c82e115dbfe7b531e16e
Wallaceness <ncw232@gmail.com>
2019-08-13 21:35:49 -0400

    Nearly there. Just that stupid image now.

--- browser/js/about_me/about_me.state.js
+++
@@ -1,7 +0,0 @@
-app.config(function($stateProvider) {
-    $stateProvider.state("about_me", {
-        url: "",
-        templateUrl: "browser/js/about_me/about_me.html",
-        controller: "about_me_ctrl"
-    })
-})
--- browser/js/links/links.state.js
+++
@@ -1,7 +0,0 @@
-app.config(function($stateProvider) {
-    $stateProvider.state('links', {
-        url: 'links',
-        templateUrl: "browser/js/links/links.html",
-        controller: "links_ctrl"
-    })
-})
